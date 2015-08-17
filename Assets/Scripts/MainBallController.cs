c7c2783c6579bb8f1bf60f463136508524df4f96
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-17 09:42:25 +0300

    added MainBallController to prefabs

--- Assets/Scripts/MainBallController.cs
+++ Assets/Scripts/MainBallController.cs
@@ -12,10 +12,21 @@ public class MainBallController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log ("enter");
-		//Destroy (other);
+		if (isPlayer) 
+		{
+			/* a collision between 2 bodies started, if the main one is a player, 
+			 * swap properties & delete the other one */
+			swapProperties(other.gameObject);
+			Destroy (other.gameObject);
+		}
+		 
 	}
 
+	/* this method will take the properties from the specified game object to this one */
+	private void swapProperties(GameObject gameObject)
+	{
+		this.GetComponent<SpriteRenderer> ().sprite = gameObject.GetComponent<SpriteRenderer> ().sprite;
+	}
 	private void checkIfPlayer()
 	{
 		/*if this object is the main player, activate the trigger */
