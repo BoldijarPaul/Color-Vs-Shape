392f27b6d5d8f5b951d8a35ef9f9ec0db7880bcc
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-17 09:37:52 +0300

    added MainBallController.cs

---
+++ Assets/Scripts/MainBallController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBallController : MonoBehaviour {
+
+	public bool isPlayer=false;
+
+	void Start () {
+		checkIfPlayer ();
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("enter");
+		//Destroy (other);
+	}
+
+	private void checkIfPlayer()
+	{
+		/*if this object is the main player, activate the trigger */
+		if(isPlayer)
+		{
+			GetComponent<Collider2D>().isTrigger=true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
